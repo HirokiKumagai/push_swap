ee78b63fbdfde538d8af0f7ea1a32d47345e810c
hirokikumagai <hiro.corp1212@gmail.com>
2022-09-06 01:31:37 +0000

    消し忘れ

--- include/push_swap.h
+++ include/push_swap.h
@@ -2,7 +2,6 @@
 # define PUSH_SWAP_H
 # include "../lib/libft/include/libft.h"
 # include "../lib/ft_printf/include/ft_printf.h"
-# include "../include/check_args.h"
 
 typedef struct s_data{
 	int				num;
