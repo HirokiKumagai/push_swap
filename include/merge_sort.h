04947d8d19472a82f7d5958a529bdbbb1c2c53ee
hirokikumagai <hiro.corp1212@gmail.com>
2022-09-15 05:45:23 +0000

    merge_sortの実装。ステップ１ 与えられたリストを半分に分割する

--- include/merge_sort.h
+++ include/merge_sort.h
@@ -6,12 +6,13 @@
 /*   By: hkumagai <hkumagai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 07:45:27 by hkumagai          #+#    #+#             */
-/*   Updated: 2022/09/14 08:11:38 by hkumagai         ###   ########.fr       */
+/*   Updated: 2022/09/14 08:42:03 by hkumagai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MERGE_SORT_H
 # define MERGE_SORT_H
+# include "../lib/libft/include/libft.h"
 t_list	**create_sorted_lst(int argc, \
 			int **arr, t_list **stack_a, t_list **stack_b);
 #endif
--- src/merge_sort.c
+++ src/merge_sort.c
@@ -6,7 +6,7 @@
 /*   By: hkumagai <hkumagai@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 07:26:23 by hkumagai          #+#    #+#             */
-/*   Updated: 2022/09/14 08:24:09 by hkumagai         ###   ########.fr       */
+/*   Updated: 2022/09/15 05:44:22 by hkumagai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,54 @@ static t_list	**store_lst(int size, int **arr, t_list **stack)
 	return (stack);
 }
 
+static void	**merge_sort(t_list **lst)
+{
+	t_list	*first_half;
+	t_list	*second_half;
+	t_list	*x;
+
+	first_half = *lst;
+	second_half = (*lst)->next;
+	if (second_half != NULL)
+		second_half = second_half->next;
+	while (second_half != NULL)
+	{
+		first_half = first_half->next;
+		second_half = second_half->next;
+		if (second_half != NULL)
+			second_half = second_half->next;
+	}
+	x = first_half->next;
+	first_half->next = NULL;
+	ft_printf("lst	%d\n", *(int *)(*lst)->content);
+	ft_printf("first_half	%d\n", *(int *)first_half->content);
+	ft_printf("x	%d\n", *(int *)x->content);
+	return (NULL);
+}
+
+// static void	merge(t_list **a, t_list **b)
+// {
+// 	t_list	**x;
+
+
+// 	while (a != NULL && b != NULL)
+// 	{
+// 		if (*(int *)(*a)->content <= *(int *)(*b)->content)
+// 		{
+// 			x->next = a;
+// 			x = x->next;
+// 			a = a->next;
+// 		}
+// 		else
+// 		{
+// 			x->next = b;
+// 			x = x->next;
+// 			b = b->next;
+// 		}
+// 	}
+
+// }
+
 t_list	**create_sorted_lst(int argc,
 						   int **arr, t_list **stack_a, t_list **stack_b)
 {
@@ -63,5 +111,6 @@ t_list	**create_sorted_lst(int argc,
 		free_all_arr(arr, size);
 		exit(1);
 	}
+	merge_sort(tmp_lst);
 	return (tmp_lst);
 }
